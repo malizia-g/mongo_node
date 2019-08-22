66b9751bc400c7428dcc50e08fb942f5495ee714
fabio-malizia <fabio.malizia@itisriva.gov.it>
2019-08-22 16:21:57 +0200

    Operazioni crud terminate

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,26 +22,61 @@ module.exports.getUserByEmail = function (req, res) {
  }
 
  module.exports.updateUserBymail = function (req, res) {
-    res.send("not implemented");
+    client.connect(err => { //Mi collego al db
+        const searchEmail = req.params.email; //Leggo il nome utente
+        const name = req.body.name;
+        const email = req.body.email;
+        const password = req.body.password;
+        var newUser = {$set:{name: name, email: email, password: password}};
+        console.log(email);
+        if (err) console.log(err.message); //Se c'è qualche errore lo stampo
+        else {
+            const collection = client.db("sample_mflix").collection("users"); //Mi connetto alla collection users
+            collection.updateOne({email:email},newUser,sendData); //Eseguo una update
+        }
+    });
+ 
+    sendData = function (err, result) {
+        if (err) {
+            console.log(err.message); //Se c'è qualche errore lo stampo
+        }
+        res.send(result);
+    }
  }
+ 
 
  module.exports.deleteUserByEmail = function (req, res) {
-    res.send("not implemented");
+    client.connect(err => { //Mi collego al db
+        const email = req.params.email; //Leggo il nome utente
+        console.log(email);
+        if (err) console.log(err.message); //Se c'è qualche errore lo stampo
+        else {
+            const collection = client.db("sample_mflix").collection("users"); //Mi connetto alla collection users
+            collection.deleteOne({email:email}, sendData); //Eseguo una delete
+        }
+    });
+ 
+    sendData = function (err, result) {
+        if (err) {
+            console.log(err.message); //Se c'è qualche errore lo stampo
+        }
+        res.send(result);
+    }
  }
 
  module.exports.createUser = function (req, res) {
-    //Leggo i parametri dal body della richiesta 
+    //Leggo i parametri dal body della richiesta e creo un nuovo utente
     console.log(req.body);
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
-    var newInfo = {name: name, email: email, password: password};
+    var newUser = {name: name, email: email, password: password};
     client.connect(err => { //Mi collego al db
         console.log(newInfo);
         if (err) console.log(err.message); //Se c'è qualche errore lo stampo
         else {
             const collection = client.db("sample_mflix").collection("users"); //Mi connetto alla collection users
-            collection.insertOne(newInfo, sendData); //Eseguo una query
+            collection.insertOne(newUser, sendData); //Eseguo una insert sul DB
         }
     });
  
@@ -51,5 +86,6 @@ module.exports.getUserByEmail = function (req, res) {
         }
         res.send(result);
     }
-    
  }
+
+ 
