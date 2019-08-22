cd4e01d66b9dbdaffca89cff87a469c2419509e2
fabio-malizia <fabio.malizia@itisriva.gov.it>
2019-08-22 16:40:13 +0200

    Codice refactored

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,91 +1,60 @@
 const MongoClient = require('mongodb').MongoClient; //Importo la libreria mongodb
 const uri = "mongodb+srv://fabio:root@cluster0-5s7o1.mongodb.net/test?retryWrites=true&w=majority"; //uri di connessione
 const client = new MongoClient(uri, { useNewUrlParser: true }); //recupero l'istanza del client
+/**
+* La funzione si connette al DB
+* @callbackQuery: E' la funzione di callback che sarà ritornata dopo la connessione al DB
+*/
+function getConnection(callbackQuery) { 
+    client.connect(err=>{
+        if (err) return callbackQuery(err);//Se c'è un errore nella connessione ritorno solo il parametro err
+        const collection = client.db("sample_mflix").collection("movies");
+        callbackQuery(null, collection); //Se riesco a connettermi non ritorno nessun errore e la collection
+    })
+ }  
+ 
+ /**
+ * Invia i dati in formato JSON al client
+ */
+ function sendData(err,result,res){
+    if (err) console.log(err.message); //Se c'è qualche errore lo stampo (dovrei spedire anche un codice di errore)
+    else res.send(result); //Altrimenti ritorno i dati
+ }
+
 
 module.exports.getUserByEmail = function (req, res) {
-    client.connect(err => { //Mi collego al db
-        const email = req.params.email; //Leggo il nome utente
-        console.log(email);
-        if (err) console.log(err.message); //Se c'è qualche errore lo stampo
-        else {
-            const collection = client.db("sample_mflix").collection("users"); //Mi connetto alla collection users
-            collection.find({email:email}).toArray(sendData); //Eseguo una query
-        }
+    const email = req.params.email; //Leggo il nome utente
+    getConnection((err,collection) => { //Se è connesso esegue questa callback
+        if (err) console.log(err.message);
+        collection.find({email:email}).toArray((err, result) => {sendData(err,result,res)});
     });
- 
-    sendData = function (err, result) {
-        if (err) {
-            console.log(err.message); //Se c'è qualche errore lo stampo
-        }
-        res.send(result);
-    }
  }
 
  module.exports.updateUserBymail = function (req, res) {
-    client.connect(err => { //Mi collego al db
-        const searchEmail = req.params.email; //Leggo il nome utente
-        const name = req.body.name;
-        const email = req.body.email;
-        const password = req.body.password;
-        var newUser = {$set:{name: name, email: email, password: password}};
-        console.log(email);
-        if (err) console.log(err.message); //Se c'è qualche errore lo stampo
-        else {
-            const collection = client.db("sample_mflix").collection("users"); //Mi connetto alla collection users
-            collection.updateOne({email:email},newUser,sendData); //Eseguo una update
-        }
+    const searchEmail = req.params.email; //Leggo email utente
+    const rb = req.body;
+    var newUser = {$set:{name: rb.name, email: rb.email, password: rb.password}};
+    getConnection((err,collection) => { //Se è connesso esegue questa callback
+        if (err) console.log(err.message);
+        collection.updateOne({email:searchEmail},newUser,{sendData(err,result,res)});
     });
- 
-    sendData = function (err, result) {
-        if (err) {
-            console.log(err.message); //Se c'è qualche errore lo stampo
-        }
-        res.send(result);
-    }
  }
  
-
  module.exports.deleteUserByEmail = function (req, res) {
-    client.connect(err => { //Mi collego al db
-        const email = req.params.email; //Leggo il nome utente
-        console.log(email);
-        if (err) console.log(err.message); //Se c'è qualche errore lo stampo
-        else {
-            const collection = client.db("sample_mflix").collection("users"); //Mi connetto alla collection users
-            collection.deleteOne({email:email}, sendData); //Eseguo una delete
-        }
+    const email = req.params.email; //Leggo email utente
+    getConnection((err,collection) => { //Se è connesso esegue questa callback
+        if (err) console.log(err.message);
+        collection.deleteOne({email:email}, {sendData(err,result,res)});
     });
- 
-    sendData = function (err, result) {
-        if (err) {
-            console.log(err.message); //Se c'è qualche errore lo stampo
-        }
-        res.send(result);
-    }
  }
 
  module.exports.createUser = function (req, res) {
-    //Leggo i parametri dal body della richiesta e creo un nuovo utente
-    console.log(req.body);
-    const name = req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
-    var newUser = {name: name, email: email, password: password};
-    client.connect(err => { //Mi collego al db
-        console.log(newInfo);
-        if (err) console.log(err.message); //Se c'è qualche errore lo stampo
-        else {
-            const collection = client.db("sample_mflix").collection("users"); //Mi connetto alla collection users
-            collection.insertOne(newUser, sendData); //Eseguo una insert sul DB
-        }
+    const rb = req.body;
+    var newUser = {$set:{name: rb.name, email: rb.email, password: rb.password}};
+    getConnection((err,collection) => { //Se è connesso esegue questa callback
+        if (err) console.log(err.message);
+        collection.insertOne(newUser,{sendData(err,result,res)});
     });
- 
-    sendData = function (err, result) {
-        if (err) {
-            console.log(err.message); //Se c'è qualche errore lo stampo su console
-        }
-        res.send(result);
-    }
  }
 
  
