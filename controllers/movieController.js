46fbb1ad3affe3416236b6708c9c40d976884343
fabio-malizia <fabio.malizia@itisriva.gov.it>
2019-08-23 16:36:41 +0200

    sistemata connessione

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,7 +1,6 @@
 const MongoClient = require('mongodb').MongoClient; //Importo la libreria mongodb
 const uri = "mongodb+srv://fabio:root@cluster0-5s7o1.mongodb.net/test?retryWrites=true&w=majority"; //uri di connessione
-const client = new MongoClient(uri, { useNewUrlParser: true }); //recupero l'istanza del client
- 
+
 module.exports.getLength_year = function (req, res){
     let num = parseInt(req.params.length);
     let num2 = parseInt(req.params.year);
@@ -11,39 +10,34 @@ module.exports.getLength_year = function (req, res){
 }
 
 module.exports.list = function (req, res) {
-    console.log("bau " + client.isConnected());
-    client.connect(err => { //Mi collego al db
+    MongoClient.connect(uri, { useNewUrlParser: true }, (err,client) => { //Mi collego al db
         let num = parseInt(req.params.num);
         if (err) console.log(err.message); //Se c'è qualche errore lo stampo
         else {
             const collection = client.db("sample_mflix").collection("movies"); //Mi connetto alla collection movies
-            collection.find().limit(num).toArray(sendData); //Eseguo la query e passo una funzione di callback
+            collection.find().limit(num).toArray((err, result) => {
+                if (err) {
+                    console.log(err.message); //Se c'è qualche errore lo stampo
+                }
+                res.send(result);
+            }); //Eseguo la query e passo una funzione di callback
         }
     });
-
-    sendData =  (err, result) => {
-        if (err) {
-            console.log(err.message); //Se c'è qualche errore lo stampo
-        }
-        res.send(result);
-    }
 }
 
 module.exports.getMovieFromTitle = function (req, res) {
-    client.connect(err => { //Mi collego al db
+    MongoClient.connect(uri, { useNewUrlParser: true }, (err,client) => { //Mi collego al db
         let titolo = req.params.title; //Leggo il titolo passato come parametro
         if (err) console.log(err.message); //Se c'è qualche errore lo stampo
         else {
             const collection = client.db("sample_mflix").collection("movies"); //Mi connetto alla collection movies
-            collection.find({'title':`${titolo}`}).toArray(sendData); //Eseguo la query e passo una funzione di callback
+            collection.find({'title':`${titolo}`}).toArray((err, result) => {
+                if (err) {
+                    console.log(err.message); //Se c'è qualche errore lo stampo
+                }
+                res.send(result);
+            }); //Eseguo la query e passo una funzione di callback
         }
     });
-
-    sendData = function (err, result) {
-        if (err) {
-            console.log(err.message); //Se c'è qualche errore lo stampo
-        }
-        res.send(result);
-    }
 }
 
